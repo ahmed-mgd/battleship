2c2973b32fc97f25213fec6f825e0f715f1233f0
ahmed-mgd <ahmedg.abdel@gmail.com>
2024-07-02 00:50:10 -0400

    Set up and implement game objects

---
+++ src/javascript/ship.js
@@ -0,0 +1,16 @@
+class Ship {
+  constructor(length) {
+    this.length = length;
+    this.numHits = 0;
+    this.vertical = true;
+    this.sunk = false;
+  }
+
+  hit() {
+    this.numHits++;
+  }
+
+  isSunk() {
+    return this.numHits === this.length;
+  }
+}
